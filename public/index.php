aefe23b42569fdfc512a61af5ebd1efc15107b1c
Nuri Engin <nuri@engin.web.tr>
2021-11-21 11:00:59 +0300

    ♻️ : Missing `create` routes implemented.

--- public/index.php
+++ public/index.php
@@ -1,16 +1,18 @@
 <?php
 
     require_once __DIR__.'/../vendor/autoload.php';
-    
+
     use app\Router;
     use app\controllers\ProductController;
 
-    $router = new Router();
+    $router = new Router($database);
+    $database = new \app\Database();
 
     $router->get('/', [ProductController::class, 'index']);
+    $router->get('/products', [ProductController::class, 'index']);
+    $router->get('/products/index', [ProductController::class, 'index']);
     $router->get('/products/create', [ProductController::class, 'create']);
-    $router->get('/products/update', [ProductController::class, 'update']);
-    $router->get('/products/delete', [ProductController::class, 'delete']);
+    $router->post('/products/create', [ProductController::class, 'create']);
 
     $router->resolve();
 ?>
